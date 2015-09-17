fccc91e111c89a53becb14bd32ff5cb3320e3c73
Neil Brayfield <neil@brayfield.uk>
2015-09-17 21:33:31 +0100

    removed autoloader as this should be handled by composer now

--- init.php
+++ init.php
@@ -1,7 +1,4 @@
 <?php
 
-// Add the markdown lib to the autoloader
-$this->loader->registerNamespaces(array('Michelf' => __DIR__.'/vendor/markdown/Michelf'), true);
-
 $di->setShared('markdown', '\Phalcana\Guide\Markdown');
 $di->setShared('userguide', '\Phalcana\Guide\Userguide');
